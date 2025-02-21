b4f5f94cc7ed3e7e019110a025dbbfb92f61fbef
Ana Carolina Durval <anacarolinadurval1@gmail.com>
2025-02-21 17:38:33 -0300

    organzação do repositório

---
+++ bugbank/data/data_login.py
@@ -0,0 +1,12 @@
+CREDENCIAIS = {
+    "invalida":{
+        "email_sem_dominio": "Teste@",
+        "email_incompleto": "Teste",
+        "senha_invalida": "Teste10",
+    },
+    "valida":{
+        "email_valido" : "teste_bugbank@gmail.com",
+        "senha_valida": "123testevalido",
+        "nome_valido": "Teste Bug Bank"        
+    }
+}
---
+++ bugbank/data/data_registro.py
@@ -0,0 +1,14 @@
+DADOS = {
+    "valido":{
+        "nome": "Carolina Teste",
+        "email": "carol_teste@gmail.com",
+        "senha": "12345Teste@",
+        "confirmacao_senha": "12345Teste@"
+    },
+    "vazio":{
+        "nome": "",
+        "email": "",
+        "senha": "",
+        "confirmacao_senha": ""
+    }    
+}
